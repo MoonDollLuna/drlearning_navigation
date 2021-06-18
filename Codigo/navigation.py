c300aec9d42c01cc5c4af8801a4bdb4a9793e58f
Luna Jiménez Fernández <luna.jimfer@gmail.com>
2021-06-18 15:39:21 +0200

    Added initial code project

---
+++ Codigo/navigation.py
@@ -0,0 +1,16 @@
+# REACTIVE NAVIGATION - MAIN CODE
+# Developed by: Luna Jimenez Fernandez
+#
+# This file contains the main structure of the reactive navigation system developed
+# (including the arguments, parameters for the simulator, data display...)
+#
+# The developed agents are contained in the "agents" folder, in separate files.
+#
+# Structure of the file:
+#   1 - Imports
+#   2 - Global variables
+#   3 -
+
+###############
+# 1 - IMPORTS #
+###############
